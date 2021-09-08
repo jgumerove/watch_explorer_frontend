a810ecf075f2f9883d4f2ecc3b82d1121b0c62bb
jgumerove <josh.gumerove@gmail.com>
2021-09-08 14:13:32 -0400

    render company content to the DOM

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const api = "http://localhost:3000/companies"
 
 
-function getApi() {
+function getCompanies() {
     fetch(api)
     .then(response => response.json())
-    .then(companies => console.log(companies));   
+    .then(companies => {
+        companies.forEach(company => {
+            const companyMarkup = `
+            <div data-id=${company.id}>
+              <h3>${company.name}</h3>
+            </div>
+            <br><br>
+            `
+            document.querySelector("#company-container").innerHTML += companyMarkup
+        })
+    });   
 }
 
-getApi()
+getCompanies()
