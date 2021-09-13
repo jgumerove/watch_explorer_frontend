735861adb078a24f0b83f212ad563e1232896c40
jgumerove <josh.gumerove@gmail.com>
2021-09-13 14:10:21 -0400

    create login functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 const api = new ApiService("http://localhost:3000")
+let user 
 
 // Company.getCompanies()
+document.querySelector("form").addEventListener("submit", handleUserSubmit)
+
+function handleUserSubmit(e){
+    e.preventDefault()
+    api.findOrCreateUser(e.target.username.value).then(userData => {
+        user = userData
+        Company.getCompanies()
+    })
+}
--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -6,4 +6,15 @@ class ApiService {
 
     getCompanies = () => fetch(this.api + "/companies").then(res => res.json())
 
+    findOrCreateUser = (username) => {
+        return fetch(this.api +"/users", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username: username}),
+        })
+        .then(response => response.json())
+    }
+
 }
